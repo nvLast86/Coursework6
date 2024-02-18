97af06d20f191bdf6f227bb8c925dcd2e57f5a4c
nvLast86 <n.lastenko@gmail.com>
2024-02-18 20:32:36 +0600

    Добавлена модель User приложение users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'пользователь'
+        verbose_name_plural = 'пользователи'
+
+    def __str__(self):
+        return f'{self.email}'
