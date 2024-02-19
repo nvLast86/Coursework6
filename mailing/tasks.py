ca9a01553e62ec5cdb27fcb4212b7f9a26228938
nvLast86 <n.lastenko@gmail.com>
2024-02-19 09:17:43 +0600

    корректировка

--- mailing/tasks.py
+++ mailing/tasks.py
@@ -5,9 +5,12 @@ from dateutil import parser
 from django.core.mail import send_mail
 from django.utils import timezone
 
-from config.celery import app
+from django.contrib.auth.views import LoginView, LogoutView
+from django.urls import path
+
+
 from config import settings
-from mailing.models import Mailing, MailingsLogs
+from mailing.models import Mailing, MailingLogs
 
 
 @app.task
@@ -23,21 +26,21 @@ def create_mailing(date_start, date_end, mailings_id, subject, body):
                 settings.EMAIL_HOST_USER,
                 [client.email for client in mailing.clients.all()],
             )
-            MailingsLogs.objects.create(
+            MailingLogs.objects.create(
                 time=timezone.now(),
                 status='SENT',
                 server_response='200',
                 mailing=mailing,
             )
         except Exception:
-            MailingsLogs.objects.create(
+            MailingLogs.objects.create(
                 time=datetime.now(),
                 status='FAILED',
                 server_response='400',
                 mailing=mailing,
             )
 
-        time.sleep(Mailings.SLEEP_DICT[mailing.frequency])
+        time.sleep(Mailing.SLEEP_DICT[mailing.frequency])
 
     mailing.status = 'FINISHED'
     mailing.save()
