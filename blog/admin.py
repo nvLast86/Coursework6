bbbfb887ae8b0127c35a45dcb210aa86014beac2
nvLast86 <n.lastenko@gmail.com>
2024-02-19 10:29:48 +0600

    Регистрация моделей в файлах admin.py приложений

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'image', 'created_at', 'views_count',)
+    search_fields = ('title', 'created_at', )
+    list_filter = ('title', 'created_at', 'views_count',)
--- mailing/admin.py
+++ mailing/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from mailing.models import Client, MailingLogs, Mailing
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'comment',)
+    search_fields = ('name', 'email',)
+    list_filter = ('name',)
+
+
+@admin.register(Mailing)
+class MailingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'time_start', 'time_end', 'frequency', 'status')
+
+
+@admin.register(MailingLogs)
+class MailingLogsAdmin(admin.ModelAdmin):
+    list_display = ('server_response', 'mailing', 'status', 'time')
