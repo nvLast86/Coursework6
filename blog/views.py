859c6c2a58d282a2d4e7d0fe816f4266a7928c9c
nvLast86 <n.lastenko@gmail.com>
2024-02-19 09:25:30 +0600

    Добавление контроллеров

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from blog.models import Post
+from blog.services import cache_posts
+
+
+class PostListView(ListView):
+    model = Post
+    data = Post.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['subjects'] = cache_posts()
+        return context_data
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_object(self, queryset=None):
+        post = super().get_object(queryset)
+        post.increase_views_count()
+        return post
+
