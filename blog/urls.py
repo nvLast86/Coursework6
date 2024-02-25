232b31f55e6aa13da218c1942b578bc4da39e8c7
nvLast86 <n.lastenko@gmail.com>
2024-02-25 10:41:49 +0600

    Добавление Blog урлов

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 
 from blog.apps import BlogConfig
-from blog.views import PostListView, PostDetailView
+from blog.views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 
 app_name = BlogConfig.name
 
 urlpatterns = [
     path('', PostListView.as_view(), name='post_list'),
     path('post/<int:pk>/<str:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('create/', PostCreateView.as_view(), name='post_create'),
+    path('posts/update/<slug:slug>/', PostUpdateView.as_view(), name='post_update'),
+    path('posts/delete/<slug:slug>/', PostDeleteView.as_view(), name='post_delete'),
 ]
 
