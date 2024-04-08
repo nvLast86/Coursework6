a9f62b8670c0e45af0b031de377b29eef7ef27ab
nlastenko <n.lastenko@gmail.com>
2024-04-08 13:55:23 +0600

    добавление шаблонов в приложение blog

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,8 @@
+from django import template
+from config import settings
+
+register = template.Library()
+
+@register.simple_tag()
+def mediapath(image_name):
+    return f'{settings.MEDIA_URL}{image_name}'
